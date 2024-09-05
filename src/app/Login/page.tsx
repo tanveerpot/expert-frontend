8707ecad088bfacb11d378d7be8d4689ade4efc4
Tanveer <tanveeritdev26@gmail.com>
2024-09-06 04:35:53 +0500

    validations added

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,23 +1,21 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { Button, Input } from "antd";
-import { useSearchParams } from "next/navigation";
-import { Select } from "antd";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import LayOut from "../Layout/layout";
 interface LayOutProps {
   role: "admin" | "dealer" | "customer";
 }
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState<LayOutProps>();
-  const [showlogin, setShowlogin] = useState(true);
-  const [showlayout, setShowlayout] = useState(false);
+  const router = useRouter();
   useEffect(() => {
-    // localStorage.removeItem("Email");
-  }, [showlayout, showlogin]);
+    const user = localStorage.getItem("Email");
+    if (user != null) {
+      router.push("/dashboard");
+    }
+  });
   const handelpassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
@@ -25,14 +23,10 @@ const Login: React.FC = () => {
   const handelemail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
-  const handleChange = (value: string) => {
-    setRole(value);
-  };
   const login = async () => {
     const postData = {
       email: email,
       password: password,
-      role: role,
     };
 
     const response = await axios.post(
@@ -40,50 +34,36 @@ const Login: React.FC = () => {
       postData
     );
     if (response.data.message == "User signed in successfully") {
+      localStorage.removeItem("Email");
       localStorage.setItem("Email", email);
-      setShowlayout(true);
-      setShowlogin(false);
+      router.push("/dashboard");
     }
   };
   return (
     <>
-      {showlogin && (
-        <div className="h-screen justify-center bg-white">
-          <h1 className="text-center text-black text-xl p-9">Login</h1>
-          <div className="w-[50%] mx-[25%] py-[3%]">
-            <Input
-              className="placeholder-black-500 border-1"
-              onChange={handelemail}
-              value={email}
-              placeholder="Enter Email"
-            />
-          </div>
-          <div className="w-[50%] mx-[25%] py-[1%]">
-            <Input
-              className="placeholder-black-500"
-              onChange={handelpassword}
-              value={password}
-              placeholder="Enter Password 123456"
-            />
-          </div>
-          <div className="w-[50%] mx-[25%] py-[1%]">
-            <Select
-              className="w-[100%]"
-              onChange={handleChange}
-              options={[
-                { value: "admin", label: <span>Admin</span> },
-                { value: "dealer", label: <span>Dealer</span> },
-                { value: "customer", label: <span>Customer</span> },
-              ]}
-            />
-            ;
-          </div>
-          <div className="w-[50%] mx-[47%] py-4">
-            <Button onClick={login}>Login</Button>
-          </div>
+      <div className="h-screen justify-center bg-white">
+        <h1 className="text-center text-black text-xl p-9">Login</h1>
+        <div className="w-[50%] mx-[25%] py-[3%]">
+          <Input
+            className="placeholder-black-500 border-1"
+            onChange={handelemail}
+            value={email}
+            placeholder="Enter Email"
+          />
         </div>
-      )}
-      {showlayout && <LayOut role={role} />}
+        <div className="w-[50%] mx-[25%] py-[1%]">
+          <Input
+            className="placeholder-black-500"
+            onChange={handelpassword}
+            value={password}
+            placeholder="Enter Password 123456"
+          />
+        </div>
+        <div className="w-[50%] mx-[25%] py-[1%]"></div>
+        <div className="w-[50%] mx-[47%] py-4">
+          <Button onClick={login}>Login</Button>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,399 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button, Layout, Menu, theme, Input } from "antd";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+const { Header, Sider, Content } = Layout;
+interface LayOutProps {
+  userRole: "admin" | "dealer" | "customer";
+}
+
+const LayOut: React.FC<LayOutProps> = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [showUsers, setShowUsers] = useState(true);
+  const [addUsers, setAddUsers] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [currentUser, setCurrentUser] = useState("");
+  const [userRole, setUseruserRole] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [customer, setCustomer] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const user = localStorage.getItem("Email");
+    if (user == null) {
+      router.push("/Login");
+    }
+    const data = { email: user };
+    const getUser = async () => {
+      const response = await axios.post(
+        "http://localhost:5000/api/login/user",
+        data
+      );
+      setUseruserRole(response.data.role);
+      setUserEmail(response.data.email);
+    };
+    getUser();
+    setIsDelete(false);
+    setName("");
+    setEmail("");
+    if (userRole == "customer") {
+      setAddUsers(false);
+      setShowUsers(false);
+      setCustomer(true);
+    }
+    const fetchData = async () => {
+      try {
+        if (userRole === "admin") {
+          const response = await axios.get("http://localhost:5000/api/dealer", {
+            headers: {
+              email: userEmail,
+            },
+          });
+          setData(response.data);
+        } else if (userRole === "dealer") {
+          const dealer = { dealer: userEmail };
+
+          const response = await axios.get(
+            "http://localhost:5000/api/customer",
+            {
+              headers: {
+                email: userEmail,
+              },
+            }
+          );
+
+          setData(response.data);
+        }
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [showUsers, userRole, isDelete, loading]);
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handleFocus = () => {
+    setEmail(currentUser);
+  };
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const dealerEmail = localStorage.getItem("Email");
+    const postData = {
+      name: name,
+      email: email,
+      creator: userEmail,
+    };
+    try {
+      if (userRole === "admin") {
+        const response = await axios.post(
+          "http://localhost:5000/api/dealer",
+          postData,
+          {
+            headers: {
+              email: userEmail,
+            },
+          }
+        );
+      } else if (userRole === "dealer") {
+        const response = await axios.post(
+          "http://localhost:5000/api/customer",
+          postData,
+          {
+            headers: {
+              email: userEmail,
+            },
+          }
+        );
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    setLoading(true);
+    setShowUsers(true);
+    setAddUsers(false);
+    setName("");
+    setEmail("");
+  };
+  const addUser = () => {
+    setShowUsers(false);
+    setAddUsers(true);
+  };
+  const editUser = (useridentity) => {
+    setCurrentUser(useridentity);
+    setShowUsers(false);
+    setIsEdit(true);
+  };
+
+  const handleCancel = () => {
+    setShowUsers(true);
+    setAddUsers(false);
+    setIsEdit(false);
+    setName("");
+    setEmail("");
+  };
+  async function deleteUser(useridentity: String) {
+    setCurrentUser(useridentity);
+    const data = { email: useridentity };
+    if (userRole === "admin") {
+      try {
+        const response = await axios.delete(
+          "http://localhost:5000/api/dealer/delete",
+          {
+            data,
+            headers: {
+              email: userEmail,
+            },
+          }
+        );
+        if (response.data.message === "Deleted dealer") {
+          setIsDelete(true);
+          setLoading(true);
+        }
+      } catch (error) {
+        console.error("Error deleting dealer:", error);
+      }
+    } else if (userRole === "dealer") {
+      try {
+        const response = await axios.delete(
+          "http://localhost:5000/api/customer/delete",
+          {
+            data,
+            headers: {
+              email: userEmail,
+            },
+          }
+        );
+      } catch (error) {
+        console.error("Error in deleting:", error);
+      }
+    }
+    setIsDelete(true);
+    setLoading(true);
+  }
+
+  const handleResponse = (data) => {
+    if (data && data.length > 0) {
+      return data.map((customer, index) => (
+        <div key={index} className="mb-3">
+          <p className="inline">{customer.name}</p>
+          <Button className="border border-gray rounded-md py-1 px-5 mx-3">
+            Subscriptions
+          </Button>
+          <button
+            className="border border-gray rounded-md py-1 px-5 mx-3 bg-blue-500 hover:text-white"
+            id={customer.email}
+            onClick={() => {
+              editUser(customer.email);
+            }}
+          >
+            Edit
+          </button>
+          <button
+            className="border border-gray rounded-md py-1 px-5 mx-3 bg-red-500 hover:text-white"
+            id={customer.email}
+            onClick={() => deleteUser(customer.email)}
+          >
+            Delete
+          </button>
+        </div>
+      ));
+    } else {
+      return <p>No data found.</p>;
+    }
+  };
+  const logout = () => {
+    localStorage.removeItem("Email");
+    router.push("/Login");
+  };
+  const handleSave = async () => {
+    const postData = {
+      oldEmail: currentUser,
+      newName: name,
+      email: email,
+    };
+    try {
+      if (userRole === "admin") {
+        const response = await axios.put(
+          "http://localhost:5000/api/dealer/update",
+          postData,
+          {
+            headers: {
+              email: userEmail,
+            },
+          }
+        );
+      } else if (userRole === "dealer") {
+        const response = await axios.put(
+          "http://localhost:5000/api/customer/update",
+          postData,
+          {
+            headers: {
+              email: userEmail,
+            },
+          }
+        );
+      }
+
+      setIsEdit(false);
+      setShowUsers(true);
+      setLoading(true);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <>
+      <Layout className="h-screen">
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={[
+              {
+                key: "1",
+                label: userRole === "admin" ? "Dealers" : "Customers",
+              },
+              {
+                key: "2",
+                label: "Settings",
+              },
+              {
+                key: "3",
+                label: "Users",
+              },
+              {
+                key: "4",
+                label: "Plans",
+              },
+            ]}
+          />
+        </Sider>
+        <Layout>
+          <Header
+            className="flex justify-between"
+            style={{ padding: 0, background: colorBgContainer }}
+          >
+            <h1 className="m-3 mt-4 font-bold text-lg capitalize ">
+              {userRole}
+            </h1>
+            <div>
+              <Button className="my-5 mr-3" onClick={logout}>
+                Logout
+              </Button>
+              <Button
+                className="my-5 mr-5 bg-blue-500 hover:text-white"
+                onClick={addUser}
+              >
+                Add {userRole === "admin" ? "Dealer" : "Customer"}
+              </Button>
+            </div>
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            {showUsers &&
+              (loading ? (
+                <p>Loading...</p>
+              ) : error ? (
+                <p>Error: {error}</p>
+              ) : (
+                <div>{handleResponse(data)}</div>
+              ))}
+            {addUsers && (
+              <div>
+                <h1 className="text-center text-xl mb-5">
+                  Add {userRole === "admin" ? "Dealer" : "Customer"}
+                </h1>
+                <Input
+                  className="mb-3"
+                  placeholder={
+                    userRole === "admin" ? "Dealer Name" : "Customer Name"
+                  }
+                  onChange={handleName}
+                  value={name}
+                />
+                <Input
+                  placeholder={
+                    userRole === "admin" ? "Dealer Email" : "Customer Email"
+                  }
+                  onChange={handleEmail}
+                  value={email}
+                />
+                <div className="mt-5">
+                  <Button
+                    className="bg-blue-500 hover:text-white"
+                    onClick={handleSubmit}
+                  >
+                    Add {userRole === "admin" ? "Dealer" : "Customer"}
+                  </Button>
+                  <Button className="ml-3" onClick={handleCancel}>
+                    Cancel
+                  </Button>
+                </div>
+              </div>
+            )}
+            {customer && <h1>Customer</h1>}
+            {isEdit && (
+              <div>
+                <h1 className="text-center text-xl mb-5">
+                  Edit {userRole === "admin" ? "Dealer" : "Customer"}
+                </h1>
+                <Input
+                  className="mb-3"
+                  placeholder={
+                    userRole === "admin" ? "Dealer Name" : "Customer Name"
+                  }
+                  onChange={handleName}
+                  value={name}
+                />
+                <Input
+                  placeholder={currentUser}
+                  onFocus={handleFocus}
+                  onChange={handleEmail}
+                  value={email}
+                />
+                <div className="mt-5">
+                  <Button
+                    className="bg-blue-500 hover:text-white"
+                    onClick={handleSave}
+                  >
+                    Save {userRole === "admin" ? "Dealer" : "Customer"}
+                  </Button>
+                  <Button className="ml-3" onClick={handleCancel}>
+                    Cancel
+                  </Button>
+                </div>
+              </div>
+            )}
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
+};
+
+export default LayOut;
